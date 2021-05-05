0724c1456358fcacf6844ce8fad671284be4b9ab
Taiga Kiyotaki <taiga@t2421.com>
2021-05-05 21:54:11 +0900

    init

---
+++ Source/JsonSerialize/JsonSerializeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JsonSerializeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JSONSERIALIZE_API AJsonSerializeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
